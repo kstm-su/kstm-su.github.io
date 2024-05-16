c161b75f67e992240e5b5aee7c7dac4c409bd204
Holoscopecheck <holoscopecheck@gmail.com>
2024-05-16 17:09:28 +0900

    socond commit for 20240515-bbq.md

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,49 +1,46 @@
 module.exports = {
-  title: 'kstm',
-  dest: 'docs/',
-  head: [
-    ['link', { rel: 'icon', type: 'image/svg', href: '/kstm.svg' }]
-  ],
+  title: "kstm",
+  dest: "docs/",
+  head: [["link", { rel: "icon", type: "image/svg", href: "/kstm.svg" }]],
   themeConfig: {
-    logo: '/kstm.svg',
+    logo: "/kstm.svg",
     search: false,
     nav: [
-      { text: 'kstmについて', link: '/aboutus' },
-      { text: 'Join', link: '/join' },
-      { text: 'Contact', link: '/contact' },
-      { text: 'Blog', link: '/posts/' }
+      { text: "kstmについて", link: "/aboutus" },
+      { text: "Join", link: "/join" },
+      { text: "Contact", link: "/contact" },
+      { text: "Blog", link: "/posts/" },
     ],
 
     // HACK: 自動でインデックスできると楽
     sidebar: [
       {
-        title: 'Blog',
-        path: '/posts/',
+        title: "Blog",
+        path: "/posts/",
         children: [
-          '/posts/',
-          '/posts/20231201-auto-build.md',
-          '/posts/geekhaku',
-          '/posts/ictsc-2020',
-          '/posts/generate_bacon',
-          '/posts/advent-calendar-2018',
-          '/posts/kstm_network',
-          '/posts/kstm-activity-2017',
-          '/posts/advent-calendar-2017',
-          '/posts/kstmztp20171208',
-          '/posts/isucon7q',
-          '/posts/20171029-candle',
-          '/posts/isucon6q',
-          '/posts/bbq-2016',
-          '/posts/kodomo-programming-nagano-2',
-          '/posts/ictsc6',
-          '/posts/icfpc-2016',
-          '/posts/kodomo-programming-nagano-1',
-          '/posts/icpc-2016-tsukuba',
-          '/posts/kodomo-programming-urugi',
-        ]
-      }
-    ]
-
-  }
-}
-
+          "/posts/",
+          "/posts/202405-bbq.md",
+          "/posts/20231201-auto-build.md",
+          "/posts/geekhaku",
+          "/posts/ictsc-2020",
+          "/posts/generate_bacon",
+          "/posts/advent-calendar-2018",
+          "/posts/kstm_network",
+          "/posts/kstm-activity-2017",
+          "/posts/advent-calendar-2017",
+          "/posts/kstmztp20171208",
+          "/posts/isucon7q",
+          "/posts/20171029-candle",
+          "/posts/isucon6q",
+          "/posts/bbq-2016",
+          "/posts/kodomo-programming-nagano-2",
+          "/posts/ictsc6",
+          "/posts/icfpc-2016",
+          "/posts/kodomo-programming-nagano-1",
+          "/posts/icpc-2016-tsukuba",
+          "/posts/kodomo-programming-urugi",
+        ],
+      },
+    ],
+  },
+};
